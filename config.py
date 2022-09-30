a8fd284345fafbb5b23f9cba30d765bbd483eaff
RajKKapadia <raajforyou@gmail.com>
2022-09-30 23:10:42 +0530

    removed input and output folders

--- config.py
+++ config.py
@@ -4,22 +4,3 @@ SAVED_MODEL_PATH = os.path.join(
     'model',
     'esrgan-tf2_1'
 )
-
-CWD = os.getcwd()
-OUTPUT_DIR = os.path.join(
-    CWD,
-    'output'
-)
-os.makedirs(
-    name=OUTPUT_DIR,
-    exist_ok=True
-)
-
-INPUT_DIR = os.path.join(
-    CWD,
-    'input'
-)
-os.makedirs(
-    name=INPUT_DIR,
-    exist_ok=True
-)
